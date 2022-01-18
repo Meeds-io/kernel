77a924681f2911651aff29b0074c71b9cfd8271a
Romain Dénarié <romain.denarie@exoplatform.com>
2022-01-18 16:36:04 +0100

    Task-49792 : Remove log4j dependency from test and compile scopes to exclusively use the same logback logging framework as in runtime

--- exo.kernel.container/src/test/java/org/exoplatform/container/log/TestLoggers.java
+++ exo.kernel.container/src/test/java/org/exoplatform/container/log/TestLoggers.java
@@ -35,73 +35,6 @@ import java.util.Properties;
 public class TestLoggers extends TestCase
 {
 
-   private final String logger = "org.slf4j.Logger";
-
-   public void _testExoLogPerformance() throws Exception
-   {
-
-      Log log = ExoLogger.getLogger(TestLoggers.class);
-
-      String confClass = "org.exoplatform.services.log.impl.Log4JConfigurator";
-
-      Properties props = new Properties();
-
-      props.put("log4j.rootLogger", "INFO, stdout, file");
-
-      props.put("log4j.appender.stdout", "org.apache.log4j.ConsoleAppender");
-      props.put("log4j.appender.stdout.threshold", "DEBUG");
-
-      props.put("log4j.appender.stdout.layout", "org.apache.log4j.PatternLayout");
-      props.put("log4j.appender.stdout.layout.ConversionPattern",
-         "%d{dd.MM.yyyy HH:mm:ss} *%-5p* [%t] %c{1}: %m (%F, line %L) %n");
-
-      props.put("log4j.appender.file", "org.apache.log4j.FileAppender");
-      props.put("log4j.appender.file.File", "target/l4j_info.log");
-
-      props.put("log4j.appender.file.layout", "org.apache.log4j.PatternLayout");
-      props.put("log4j.appender.file.layout.ConversionPattern",
-         "%d{dd.MM.yyyy HH:mm:ss} *%-5p* [%t] %c{1}: %m (%F, line %L) %n");
-
-      new LogConfigurationInitializer(logger, confClass, props);
-
-      log.info("Performance test.");
-   }
-
-   public void _testLog4j() throws Exception
-   {
-
-      Log log = ExoLogger.getLogger(TestLoggers.class);
-
-      String confClass = "org.exoplatform.services.log.impl.Log4JConfigurator";
-
-      Properties props = new Properties();
-
-      props.put("log4j.rootLogger", "DEBUG, stdout, file");
-
-      props.put("log4j.appender.stdout", "org.apache.log4j.ConsoleAppender");
-      props.put("log4j.appender.stdout.threshold", "DEBUG");
-
-      props.put("log4j.appender.stdout.layout", "org.apache.log4j.PatternLayout");
-      props.put("log4j.appender.stdout.layout.ConversionPattern",
-         "%d{dd.MM.yyyy HH:mm:ss} *%-5p* [%t] %c{1}: %m (%F, line %L) %n");
-
-      props.put("log4j.appender.file", "org.apache.log4j.FileAppender");
-      props.put("log4j.appender.file.File", "target/l4j_info.log");
-
-      props.put("log4j.appender.file.layout", "org.apache.log4j.PatternLayout");
-      props.put("log4j.appender.file.layout.ConversionPattern",
-         "%d{dd.MM.yyyy HH:mm:ss} *%-5p* [%t] %c{1}: %m (%F, line %L) %n");
-
-      LogConfigurationInitializer initializer = new LogConfigurationInitializer(logger, confClass, props);
-      log.info("LOG4J Tests");
-      logOut(log);
-
-      initializer.setProperty("log4j.rootLogger", "INFO, stdout, file");
-      initializer.setProperty("log4j.appender.file.File", "target/l4j_debg.log");
-
-      logOut(log);
-
-   }
 
    public void testLog4jContainer() throws Exception
    {
@@ -117,50 +50,6 @@ public class TestLoggers extends TestCase
 
    }
 
-   /**
-    * To launch this test: 1. remove Log4jConfogurator from
-    * org.exoplatform.services.log. 2. remove log4j dependency from
-    * exo.kernel.commons. 3. replace slf4j-log4j12 with slf4j-jcl in
-    * exo.kernel.commons dependencies.
-    */
-   public void _testJCLLog() throws Exception
-   {
-
-      String confClass = "org.exoplatform.services.log.impl.Jdk14Configurator";
-
-      Properties props = new Properties();
-
-      props.put("handlers", "java.util.logging.ConsoleHandler,java.util.logging.FileHandler");
-      props.put(".level", "INFO");
-      props.put("java.util.logging.ConsoleHandler.level", "ALL");
-      props.put("java.util.logging.FileHandler.pattern", "./target/java%u.log");
-      props.put("java.util.logging.FileHandler.formatter", "java.util.logging.SimpleFormatter");
-
-      new LogConfigurationInitializer(logger, confClass, props);
-      Log log = ExoLogger.getLogger(TestLoggers.class);
-
-      log.info("JCL Tests");
-      logOut(log);
-
-   }
-
-   /**
-    * To launch this test: 1. remove Log4jConfogurator from
-    * org.exoplatform.services.log. 2. remove log4j dependency from
-    * exo.kernel.commons. 3. replace slf4j-log4j12 with slf4j-jcl in
-    * exo.kernel.commons dependencies. 4. Comment Log4J logger configuration and
-    * uncomment JDK14 logger configuration in conf.portal/test-configuration.xml.
-    */
-   public void _testJCLContainer() throws Exception
-   {
-
-      PortalContainer.getInstance();
-      Log log = ExoLogger.getLogger(TestLoggers.class);
-
-      log.info("JCL Container Tests");
-      logOut(log);
-
-   }
 
    private void logOut(Log log)
    {
