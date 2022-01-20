fc0f97eb2d9f42b8cf459cea773614fadf6e34eb
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2022-01-20 14:34:20 +0100

    TASK-53370 Upgrade to Tomcat 9

--- exo.kernel.container/src/test/java/org/exoplatform/container/configuration/TestConfigurationManagerImpl.java
+++ exo.kernel.container/src/test/java/org/exoplatform/container/configuration/TestConfigurationManagerImpl.java
@@ -606,5 +606,40 @@ public class TestConfigurationManagerImpl extends TestCase
       public String getVirtualServerName() {
         return null;
       }
+
+      @Override
+      public Dynamic addJspFile(String servletName, String jspFile) {
+        return null;
+      }
+
+      @Override
+      public int getSessionTimeout() {
+        return 0;
+      }
+
+      @Override
+      public void setSessionTimeout(int sessionTimeout) {
+        
+      }
+
+      @Override
+      public String getRequestCharacterEncoding() {
+        return null;
+      }
+
+      @Override
+      public void setRequestCharacterEncoding(String encoding) {
+        
+      }
+
+      @Override
+      public String getResponseCharacterEncoding() {
+        return null;
+      }
+
+      @Override
+      public void setResponseCharacterEncoding(String encoding) {
+        
+      }
    }
 }
