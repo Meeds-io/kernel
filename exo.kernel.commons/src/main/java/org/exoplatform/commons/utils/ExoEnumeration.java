c4b8fbddae4f5465bb24047b4bcfb47069f2b8fc
Boubaker Khanfir <boubaker.khanfir@gmail.com>
2025-05-25 08:40:15 +0100

    Add Java and XML Files License Headers - Meeds-io/meeds#3324 This change will add LGPL License Header and clean useless File Headers.

--- exo.kernel.commons/src/main/java/org/exoplatform/commons/utils/ExoEnumeration.java
+++ exo.kernel.commons/src/main/java/org/exoplatform/commons/utils/ExoEnumeration.java
@@ -1,35 +1,29 @@
-/*
- * Copyright (C) 2009 eXo Platform SAS.
+/**
+ * This file is part of the Meeds project (https://meeds.io/).
  *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
+ * Copyright (C) 2020 - 2025 Meeds Association contact@meeds.io
  *
- * This software is distributed in the hope that it will be useful,
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 package org.exoplatform.commons.utils;
 
 /** * Jul 11, 2004 
- * @author Tuan Nguyen
- * @version $Id: ExoEnumeration.java,v 1.3 2004/07/13 02:46:19 tuan08 Exp $
  */
 
 import java.util.Enumeration;
 import java.util.Iterator;
 
-/**
- * @author Ove Ranheim (oranheim@users.sourceforge.net)
- * @since Nov 9, 2003 4:01:29 PM
- */
 public class ExoEnumeration implements Enumeration
 {
    private Iterator iterator_;
